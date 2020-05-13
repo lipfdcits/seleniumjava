285a8c1326289ee6536c321418bc99e23cab7f4c
lipf <lipf_dcits@163.com>
2020-05-14 07:10:21 +0800

    测试jenkins素材

--- src/test/java/com/qingxiaoguo/TestGit.java
+++ src/test/java/com/qingxiaoguo/TestGit.java
@@ -1,4 +1,27 @@
 package com.qingxiaoguo;
 
+import org.junit.Before;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class TestGit {
+
+    @Test
+    public void zhushi1(){
+        System.out.println("这是@Test1注释");
+    }
+    @Test
+    public void zhushi2(){
+        System.out.println("这是@Test2注释");
+    }
+    @BeforeTest
+    public void zhushi3(){
+        System.out.println("这是@BeforeTest1注释");
+    }
+    @AfterTest
+    public void zhushi4(){
+        System.out.println("这是@AfterTest注释");
+    }
+
 }
